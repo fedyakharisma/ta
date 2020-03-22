6752502611c4679726a8cbe846af422ad1721bb2
fedyakharisma <48073474+fedyakharisma@users.noreply.github.com>
2020-03-23 01:29:09 +0700

    Update v_tablePasien.php

--- application/views/pasien/v_tablePasien.php
+++ application/views/pasien/v_tablePasien.php
@@ -58,7 +58,7 @@
                                             <td><?php echo $data->tempat; ?>, <?php echo $data->tanggal_lahir; ?></td>
                                             <td><?php echo $data->nip_ktp_nrp; ?></td>
                                             <td>
-                                                <div class="row"><a href="">Detail</a></div>
+                                                <div class="row"><a href="" data-toggle="modal" data-target="#modal_detail<?php echo $data->no_rm; ?>">Detail</a></div>
                                                 <div class="row"><a href="">Ubah</a></div>
                                                 <div class="row"><a href="<?php echo base_url('Pasien/hapusPasien/' . $data->no_rm) ?>" onclick="return confirm('Hapus <?php echo $data->nama ?>?')" >Hapus</a></div>
                                             </td>
@@ -203,6 +203,158 @@
                     </div>
                     <!--END MODAL ADD BARANG-->
 
+<?php
+foreach ($pasien as $data) {
+    $tanggaldaftar = $data->tanggal_daftar;
+    $norm = $data->no_rm;
+    $nobpjs = $data->no_bpjs;
+    $nama = $data->nama;
+    $tempat = $data->tempat;
+    $tanggallahir = $data->tanggal_lahir;
+    $nipktpnrp = $data->nip_ktp_nrp;
+    $alamatt= $data->alamat;
+    $keterangan = $data->keterangan;
+    $jeniskelamin = $data->jenis_kelamin;
+?>
+                    
+                    <!-- ============ MODAL DETAIL PASIEN =============== -->
+                    <div class="modal fade" tabindex="-1" role="dialog" id="modal_detail<?php echo $norm; ?>" aria-labelledby="largeModal" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title" id="myModalLabel">Detail Pasien <?php echo $norm; ?></h3>
+                        </div>
+                        <form class="form-horizontal" method="" role="form" action="">
+                            <div class="modal-body">
+                                
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" > Tanggal Daftar</label>
+                                            <div class="col-xs-8">
+                                                <input name="namapasien" class="form-control" type="text" placeholder="Cth. Fedya Kharisma Putra" disabled value="<?php echo $tanggaldaftar; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >No RM</label>
+                                            <div class="col-xs-8">
+                                                <input name="nobpjs" class="form-control" type="text" placeholder="Cth. 000xxxxxxxxxx" disabled value="<?php echo $norm; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >Nama Pasien</label>
+                                            <div class="col-xs-8">
+                                                <input name="namapasien" class="form-control" type="text" placeholder="Cth. Fedya Kharisma Putra" disabled value="<?php echo $nama; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >No BPJS</label>
+                                            <div class="col-xs-8">
+                                                <input name="nobpjs" class="form-control" type="text" placeholder="Cth. 000xxxxxxxxxx" disabled value="<?php echo $nobpjs; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >Tempat Lahir</label>
+                                            <div class="col-xs-8">
+                                                <input name="tempat" class="form-control" type="text" placeholder="Cth.Probolinggo" disabled value="<?php echo $tempat; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >Tanggal Lahir</label>    
+                                            <div class="col-xs-4">
+                                                <input name="tanggallahir" class="form-control" type="date" placeholder="Tanggal Lahir..." disabled value="<?php echo $tanggallahir; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >Jenis Kelamin</label>
+                                            <div class="col-xs-8">
+                                                 <select name="jk" class="form-control" disabled value="">
+                                                    <option value="" ><?php echo $jeniskelamin; ?></option>
+                                                 </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="control-label col-xs-3" >NIK/KTP/NRP</label>
+                                            <div class="col-xs-8">
+                                                <input name="nipktpnrp" class="form-control" type="text" placeholder="Masukkan NIK/KTP/NRP..." disabled value="<?php echo $nipktpnrp; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="control-label col-xs-3" >Alamat</label>
+                                    <div class="col-xs-8">
+                                        <textarea name="alamat" class="form-control" placeholder="Cth. Jl.Sudirxxxxx...." disabled value=""><?php echo $alamatt; ?></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="control-label col-xs-3" >Keterangan</label>
+                                    <div class="col-xs-8">
+                                        <input name="keterangan" class="form-control" type="text" placeholder="Keterangan..." disabled value="<?php echo $keterangan; ?>">
+                                    </div>
+                                </div>
+
+                                <!--<div class="form-group">
+                                    <label class="control-label col-xs-3" >Satuan</label>
+                                    <div class="col-xs-8">
+                                         <select name="satuan" class="form-control" required>
+                                            <option value="">-PILIH-</option>
+                                            <option value="Unit">Unit</option>
+                                            <option value="Kotak">Kotak</option>
+                                            <option value="Botol">Botol</option>
+                                            <option value="Sachet">Sachet</option>
+                                            <option value="Pcs">Pcs</option>
+                                            <option value="Dus">Dus</option>
+                                         </select>
+                                    </div>
+                                </div>
+             
+                                <div class="form-group">
+                                    <label class="control-label col-xs-3" >Harga</label>
+                                    <div class="col-xs-8">
+                                        <input name="harga" class="form-control" type="text" placeholder="Harga..." required>
+                                    </div>
+                                </div>
+                                 -->   
+                            </div>
+             
+                            <div class="modal-footer">
+                                <button class="btn btn-default" data-dismiss="modal" aria-hidden="true">Tutup</button>
+                                
+                            </div>
+                        </form>
+                        </div>
+                        </div>
+                    </div>
+                    <!--END MODAL DETAIL PASIEN-->
+                    <?php } ?>
                 </div>
                 <!-- /.card -->
             </div>
